b647fc590b380651a06ac3fbfc9dbb4ce0064286
Ruan <rkruger270@gmail.com>
2021-06-18 15:51:30 +0200

    Initial commit

---
+++ NUnitTest.DotDigitalChallenge/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NUnitTest.DotDigitalChallenge
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
